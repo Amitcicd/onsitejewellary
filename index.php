df4075fa05c363c274ee0f124d6174045c3d10e0
Amitcicd <Amit@webminesllc.us>
2025-09-03 14:07:05 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	
+	<!-- PAGE TITLE HERE -->
+	<title>On Set Jewellery- Lunching Soon,</title>
+	
+	<!-- META -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="author" content="Webmines LLC">
+<meta name="robots" content="On Set Jewellery">
+<meta property="og:title" content="On Set Jewellery">
+<meta property="og:description" content="On Set Jewellery">
+<meta property="og:image" content="social-image.png">
+<meta name="format-detection" content="telephone=no">
+
+	
+	<!-- FAVICONS ICON -->
+	<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.png">
+	
+
+
+	<!-- MOBILE SPECIFIC -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	
+	<!-- STYLESHEETS -->
+	<link rel="stylesheet" type="text/css" href="css/plugins.css">
+	<link rel="stylesheet" type="text/css" href="css/style.min.css">
+	<link rel="stylesheet" type="text/css" href="css/templete.min.css">
+	<link rel="stylesheet" type="text/css" class="skin" href="css/skin/skin-9.css">
+	<link rel="stylesheet" type="text/css" href="css/jquery.localizationTool.css">
+	<link href="https://fonts.googleapis.com/css?family=DM+Sans:400,400i,500,500i,700,700i&amp;display=swap" rel="stylesheet">	
+	
+</head>
+<body id="bg" class="font-poppins">
+<div id="loading-area"><div></div></div>
+<div class="page-wraper">
+	
+	<!-- Side Nav End-->
+	<!-- Coming Soon Contant -->
+	<div class="dez-coming-soon style-9 wow fadeIn" id="particles-js" data-wow-duration="0.80s" data-wow-delay="0.50s" style="background-image:url(https://joyfak.org/images/background/bg1.jpg)">
+		<div class="clearfix dez-coming-bx">
+			<div class="dez-content top-left center-md">
+				<div class="logo wow fadeInUp bottom-left" data-wow-duration="1.5s" data-wow-delay="1.0s">
+					<a href="index.html"><img src="" alt=""></a>
+				</div>
+				<div class="countdown">
+					<div class="date wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="1.0s">
+						<span class="days time"></span>
+						<span>Days</span>
+					</div>
+					<div class="date wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="1.3s">
+						<span class="hours time"></span>
+						<span>Hours</span>
+					</div>
+					<div class="date wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="1.6s">
+						<span class="mins time"></span>
+						<span>Minutes</span>
+					</div>
+					<div class="date wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="1.9s">
+						<span class="secs time"></span>
+						<span>Second</span>
+					</div>
+				</div>
+				<h2 class="dez-title ml2 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="1.5s"><span>Launching Soon. Stay Tuned! </span><br><span>Elegance Redefined. Beauty that Lasts Forever.</span></h2>
+				<div class="dez-coming-btn wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="2.2s">
+				</div>
+			</div>
+			<div class="text-right bottom-right">
+				<ul class="dez-social-icon social-2">
+					<li class="wow fadeInRight" data-wow-duration="1.5s" data-wow-delay="1.0s"><a target="_blank" href=""><i class="fa fa-facebook"></i><span>Facebook</span></a></li>
+					<li class="wow fadeInRight" data-wow-duration="1.5s" data-wow-delay="1.3s"><a target="_blank" href=""><i class="fa fa-twitter"></i><span>Twitter</span></a></li>
+					<li class="wow fadeInRight" data-wow-duration="1.5s" data-wow-delay="1.6s"><a target="_blank" href=""><i class="fa fa-instagram"></i><span>Instagram</span></a></li>
+				</ul>
+				<p class="copyright-text wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="2s"><span>© 2023 - Power By</span><span class="heart"><i></i></span>By <a href="javascript:void(0)" class="made-by">Webmines LLC</a></p>
+			</div>
+		</div>
+	</div>
+	<!-- Coming Soon Contant -->
+	
+</div>
+<!-- JavaScript  files ========================================= -->
+<script src="js/jquery.min.js"></script><!-- JQUERY.MIN JS -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script><!-- BOOTSTRAP.MIN JS -->
+<script src="plugins/countdown/jquery.countdown.js"></script><!-- JQUERY COUNTDOWN -->
+<script src="plugins/scroll/scrollbar.min.js"></script><!-- SCROLLBAR -->
+<script src="plugins/perfect-scrollbar/js/perfect-scrollbar.min.js"></script><!-- SCROLLBAR -->
+<script src="plugins/imagesloaded/imagesloaded.js"></script><!-- IMAGESLOADED -->
+<script src="plugins/masonry/masonry-4.2.2.js"></script><!-- MASONRY -->
+<script src="plugins/masonry/isotope.pkgd.min.js"></script><!-- MASONRY -->
+<script src="plugins/lightgallery/js/lightgallery-all.min.js"></script><!-- LIGHTGALLERY -->
+<script src="plugins/particles/particles.js"></script><!-- PARTICLES JS  -->
+<script src="plugins/particles/particles.app.js"></script><!-- PARTICLES JS  -->
+<script src="js/jquery.localizationTool.js"></script><!-- localizationTool JS -->
+<script src="js/translator.js"></script><!-- Translator JS -->
+<script src="js/custom.js"></script><!-- CUSTOM FUCTIONS  -->
+<script src="js/dz.ajax.js"></script><!-- CONTACT JS  -->
+<script src="js/wow.js"></script><!-- WOW JS -->
+<script src="plugins/anime/anime.min.js"></script><!-- CUSTOM FUCTIONS  -->
+<script src='../../www.google.com/recaptcha/api.js'></script> <!-- Google API For Recaptcha  -->
+<script src="plugins/switcher/switcher.js"></script><!-- switcher JS -->
+</body>
+
+</html>
